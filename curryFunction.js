b28bce4d0cf143ce41efd9248d1a518a46363c13
Sai Myo Myat <saimyomyat511@gmail.com>
2022-10-12 18:29:31 +0630

    curryFunction

--- curryFunction.js
+++ curryFunction.js
@@ -12,3 +12,12 @@ curry("pork")("burger")("cheese")
 
 //another example 
 const curry2 = ingredient1 => ingredient2 => ingredient3 => console.log("ingredients: ", ingredient1, ingredient2, ingredient3)
+
+//partialy using
+const multiply = x => y => x * y;
+
+const multipliedByTen = multiply(10);
+console.log(multipliedByTen)
+
+const result = multipliedByTen(2);
+console.log(result)
